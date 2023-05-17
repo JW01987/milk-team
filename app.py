969e4926b8c2c6b99ca988ed1329de468ca9ac99
Jean <etnpfj@gmail.com>
2023-05-17 09:11:14 +0900

    인덱스 받기 전 커밋

--- app.py
+++ app.py
@@ -2,13 +2,23 @@ from flask import Flask, render_template, request, jsonify
 from pymongo import MongoClient
 import certifi
 ca = certifi.where()
-#client = MongoClient('',tlsCAFile=ca)몽고디비 url넣기
-# db = client.컬렉션 이름넣기
+client = MongoClient('mongodb+srv://mini:test@cluster0.dyeflfq.mongodb.net/?retryWrites=true&w=majority',tlsCAFile=ca)
+db = client.milk
 app = Flask(__name__)
 
 @app.route('/')
 def home():
    return render_template('index.html')
+
+# @app.route('/test',methods=["POST"])
+# def test():
+#    test = request.form['test']
+#    db.fan.insert_one({"test":test})
+#    return jsonify({'msg': '저장완료!'})
+
+@app.route('/jowon')
+def jowon():
+   return render_template('jowon.html')
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5001, debug=True)
