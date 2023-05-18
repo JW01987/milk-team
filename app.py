463aa9af93ddcdc734dcbb45d4395296536c793c
k0723 <kimbum7824@gmail.com>
2023-05-18 09:14:01 +0900

    "personal_page 변수이름, app.py개인api 수정"
    This reverts commit fc08f3b57d3eb94b67e085ddcf53445783fabc24.

--- app.py
+++ app.py
@@ -75,23 +75,22 @@ def getmember():
 # ======여기부터는 개인페이지 API입니다===========
 
 
-@app.route("/comments", methods=["POST"])
+@app.route("/user/comment", methods=["POST"])
 def guestbook_post():
     name_receive = request.form["name_give"]
     comment_receive = request.form["comment_give"]
 
-    doc = {"member_id": 1,
-            "nick_name": name_receive,
-            "comment": comment_receive}
+    doc = {"master": "Changbum Kim", "name": name_receive, "comment": comment_receive}
 
-    db.comments.insert_one(doc)
+    db.team.insert_one(doc)
     return jsonify({"msg": "방명록 작성 완료!"})
 
 
-@app.route("/members/comments/1", methods=["GET"])
+@app.route("/all_comment", methods=["GET"])
 def guestbook_get():
-    all_comments = list(db.comments.find({"member_id": 1}, {"_id": False}))
-    all_id = list(db.comments.find({}, {"_id": ObjectId()}))
+    all_comments = list(db.team.find({"master": "Changbum Kim"}, {"_id": False}))
+    all_id = list(db.team.find({"master": "Changbum Kim"}, {"_id": ObjectId()}))
+
     return jsonify({"result": all_comments, "resultid": obejectInCoder(all_id)})
 
 
